4a0cc26b1f20319e96d6d65626c30c6eab2a11be
Michel Vuijlsteke <michel@zog.org>
2025-11-30 22:08:26 +0100

    service worker issues

--- sw.js
+++ sw.js
@@ -1,16 +1,40 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('timebeat-store').then((cache) => cache.addAll([
-      './',
-      './index.html',
-      './timebeat.png',
-      './IBMPlexMono-Regular.ttf',
-    ])),
+const CACHE_NAME = 'timebeat-store-v2';
+const CORE_ASSETS = [
+  './',
+  './index.html',
+  './timebeat.png',
+  './IBMPlexMono-Regular.ttf',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(CORE_ASSETS))
+      .then(() => self.skipWaiting()),
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request)),
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)),
+    )).then(() => self.clients.claim()),
+  );
+});
+
+// Network-first so refreshed visits always see the latest content, falling back to cache offline.
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const clonedResponse = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clonedResponse));
+        return response;
+      })
+      .catch(() => caches.match(event.request)),
   );
 });
